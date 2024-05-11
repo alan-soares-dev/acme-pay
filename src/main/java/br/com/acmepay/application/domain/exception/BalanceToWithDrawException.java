050ddf7e1bc0cd3a4bbd39c433a2bb56288dbc43
Alan Soares de Sousa <jpalanss@academicos.com>
2024-05-11 09:57:34 -0300

    Nova estrutura

--- src/main/java/br/com/acmepay/application/domain/exception/BalanceToWithDrawException.java
+++
@@ -1,7 +0,0 @@
-package br.com.acmepay.application.domain.exception;
-
-public class BalanceToWithDrawException extends Exception {
-    public BalanceToWithDrawException(String message) {
-        super(message);
-    }
-}
--- src/main/java/br/com/acmepay/application/domain/exception/DocumentInvalidException.java
+++
@@ -1,7 +0,0 @@
-package br.com.acmepay.application.domain.exception;
-
-public class DocumentInvalidException extends Exception {
-    public DocumentInvalidException(String message) {
-        super(message);
-    }
-}
