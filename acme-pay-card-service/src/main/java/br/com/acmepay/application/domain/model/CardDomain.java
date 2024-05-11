7e122b4a127bdbdd8d916aa8287606ea5fa94cf6
Alan Soares <jpalanss@gmail.com>
2024-05-11 10:59:26 -0300

    Card and Notification Domain

---
+++ acme-pay-card-service/src/main/java/br/com/acmepay/application/domain/model/CardDomain.java
@@ -0,0 +1,12 @@
+package br.com.acmepay.application.domain.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CardDomain {
+    private Long id;
+    private String flag;
+    private BigDecimal card_limit;
+}
